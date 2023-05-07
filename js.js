c75b961f64b3dc61725f72aaf30876d6f0d0b0cd
Tarek salah <64745962+tarek-98@users.noreply.github.com>
2023-05-07 22:39:49 +0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,11 @@
+let col=
+`
+    <div class="col">
+        <img src="images/footer-logo.png" alt="">
+        <p>There are many variat ions of passages of L orem Ipsum available , but the majority h ave suffered altera tion in some form, by</p>
+    </div>
+`;
+document.write(col);
+document.write(col);
+document.write(col);
+document.write(col);
